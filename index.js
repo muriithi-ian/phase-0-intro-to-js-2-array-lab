e527172f1fda4fa684d7ec88734b5d8eb803f0d6
Muriithi Ian <imuriithiian@gmail.com>
2022-08-03 12:40:10 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(cat) {
+  cats.push(cat);
+}
+
+function destructivelyPrependCat(cat) {
+  cats.unshift(cat);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+function appendCat(cat) {
+  return [...cats, cat];
+}
+
+function prependCat(cat) {
+  return [cat, ...cats];
+}
+
+function removeLastCat() {
+  return cats.slice(0, -1);
+}
+
+function removeFirstCat() {
+  return cats.slice(1);
+}
